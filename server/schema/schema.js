525c21d054900fdd71de74f3d8398902a6660c99
Alexey <alexei20293@yahoo.com>
2023-05-30 22:41:28 +0300

    add directors and connection between 2 GQL models

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,10 +1,21 @@
 const graphql = require("graphql");
 
-const { GraphQLSchema, GraphQLObjectType, GraphQLString, GraphQLID } = graphql;
+const {
+  GraphQLSchema,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLID,
+  GraphQLInt,
+} = graphql;
 
 const movies = [
-  { id: "1", name: "Pulp Fiction", genre: "Crime" },
-  { id: "2", name: "1984", genre: "Sci-Fi" },
+  { id: "1", name: "Pulp Fiction", genre: "Crime", directorId: "2" },
+  { id: "2", name: "1984", genre: "Sci-Fi", directorId: "1" },
+];
+
+const directors = [
+  { id: "1", name: "James McTeigue", age: 51 },
+  { id: "2", name: "Quentin Tarantino", age: 55 },
 ];
 
 const MovieType = new GraphQLObjectType({
@@ -13,6 +24,21 @@ const MovieType = new GraphQLObjectType({
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     genre: { type: GraphQLString },
+    director: {
+      type: DirectorType,
+      resolve(parent, args) {
+        return directors.find((el) => el.id === parent.id);
+      },
+    },
+  }),
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: "Director",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    age: { type: GraphQLInt },
   }),
 });
 
@@ -25,7 +51,16 @@ const Query = new GraphQLObjectType({
         id: { type: GraphQLID },
       },
       resolve(parent, args) {
-        return movies.find((m) => m.id == args.id);
+        return movies.find((el) => el.id === args.id);
+      },
+    },
+    director: {
+      type: DirectorType,
+      args: {
+        id: { type: GraphQLID },
+      },
+      resolve(parent, args) {
+        return directors.find((el) => el.id === args.id);
       },
     },
   },
